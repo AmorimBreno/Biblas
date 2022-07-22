5c2bc11334a0e2782714470e2ef131e9ceabd910
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-22 11:52:04 -0300

    mudando path do 'themes/themes_colors.dart'

--- lib/modules/busca/pages/busca.dart
+++ lib/modules/busca/pages/busca.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../../../widgets/app_bar.dart';
+
 class BuscaPage extends StatelessWidget {
   const BuscaPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: PreferredSize(preferredSize: Size(52, 52), child: AppBarMaua()),
       backgroundColor: Colors.black,
     );
   }
--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import '../themes/themes_colors.dart';
 
 class AppBarMaua extends StatelessWidget {
   const AppBarMaua({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return AppBar(
+      backgroundColor: AppColors.darkblue,
     );
   }
 }
