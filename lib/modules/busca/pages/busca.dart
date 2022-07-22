4cbe4c193ad1327fe878e27fb092d6a5cbb302ef
Breno <brenoamorim11@gmail.com>
2022-07-22 19:56:13 -0300

    Versão 1 da barar de pesquisa

--- lib/modules/busca/pages/busca.dart
+++ lib/modules/busca/pages/busca.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:projeto_biblas/modules/widgets/search_bar.dart';
 
 class BuscaPage extends StatelessWidget {
   const BuscaPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-    );
+    return const SearchBar();
   }
 }
