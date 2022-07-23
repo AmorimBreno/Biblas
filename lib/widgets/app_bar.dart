54da51e356e051bc533fd59d89cba200bc5d28b2
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-23 11:40:13 -0300

    .

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -4,10 +4,33 @@ import '../themes/themes_colors.dart';
 
 class AppBarMaua extends StatelessWidget {
   const AppBarMaua({Key? key}) : super(key: key);
+  final String pathLogoMaua = "assets/images/logo_maua.png";
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: AppColors.darkblue,
+    return Center(
+      child: AppBar(
+        backgroundColor: AppColors.darkblue,
+        title: const Text(''),
+        actions: [
+          Column(
+            children: [Container()],
+          )
+        ],
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Container(
+            height: 128,
+            width: 128,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  filterQuality: FilterQuality.high,
+                  image: AssetImage(pathLogoMaua)),
+            ),
+          ),
+        ),
+
+        //leading: ,
+      ),
     );
   }
 }
