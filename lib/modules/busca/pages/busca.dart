e419226ef0122a664c62b167cd25401f2af07c72
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-24 22:59:40 -0300

    appbar terminada

--- lib/modules/busca/pages/busca.dart
+++ lib/modules/busca/pages/busca.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
-import '../../../widgets/app_bar.dart';
+import 'package:projeto_biblas/widgets/app_bar.dart';
 
 class BuscaPage extends StatelessWidget {
   const BuscaPage({Key? key}) : super(key: key);
@@ -10,7 +9,8 @@ class BuscaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: PreferredSize(preferredSize: Size(52, 52), child: AppBarMaua()),
+      //appBar: PreferredSize(preferredSize: Size(52, 64), child: AppBarMaua()),
+      body: AppBarMaua(),
       backgroundColor: Colors.black,
     );
   }
--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,47 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
-import '../themes/themes_colors.dart';
+class AppBarTest extends StatelessWidget {
+  const AppBarTest({Key? key}) : super(key: key);
 
-class AppBarMaua extends StatelessWidget {
-  const AppBarMaua({Key? key}) : super(key: key);
-  final String pathLogoMaua = "assets/images/logo_maua.png";
-  final String pathLogoMauaNet = "assets/images/logo_maua_net.png";
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AppBar(
-        backgroundColor: AppColors.darkblue,
-        title: const Text(''),
-        actions: [
-          Column(
-            children: [
-              Image(
-                width: 128,
-                image: AssetImage(pathLogoMauaNet),
-              ),
-              Text(
-                  style: TextStyle(fontSize: 8),
-                  "XXXXXXXXXX - XXXXXX@hotmail.com"),
-              Text(
-                  style: TextStyle(fontSize: 8),
-                  "RA: XX.XXXXX-X | Celular: (XX)XXXXX-XXXX"),
-              Text(
-                  style: TextStyle(fontSize: 8),
-                  "| Alterar Informações | | Sair |")
-            ],
-          ),
-        ],
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Image(
-            height: 128,
-            width: 256,
-            image: AssetImage(pathLogoMaua),
-          ),
-        ),
-
-        //leading: ,
-      ),
-    );
+    return const Image(image: AssetImage("assets/images/AppBar.png"));
   }
 }
