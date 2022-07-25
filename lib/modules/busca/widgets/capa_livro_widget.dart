18357f14a2a2bded3be30d513efc5e1283142296
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-24 23:32:26 -0300

    livros

---
+++ lib/modules/busca/widgets/capa_livro_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import '../../../data_base/modules/livro/livro.dart';
+
+class CapaLivro extends StatelessWidget {
+  final Livro livro;
+  // ignore: use_key_in_widget_constructors
+  const CapaLivro({required this.livro});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(
+          livro.imagemLivro,
+          height: 198,
+          width: 128,
+          filterQuality: FilterQuality.high,
+        ),
+        SizedBox(
+          width: 168,
+          child: Text(
+            livro.titulo,
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/widgets/app_bar_widget.dart
+++ lib/widgets/app_bar_widget.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class AppBarTest extends StatelessWidget {
-  const AppBarTest({Key? key}) : super(key: key);
+class AppBarMaua extends StatelessWidget {
+  const AppBarMaua({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
