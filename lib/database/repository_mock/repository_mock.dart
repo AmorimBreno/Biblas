b614461224e222c9b5ee48dc2b8d246d5fe2f5ce
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-26 12:32:12 -0300

    adicionando os outros livros

--- lib/database/repository_mock/repository_mock.dart
+++ lib/database/repository_mock/repository_mock.dart
@@ -99,7 +99,66 @@ class RepositoryMock {
         "Acidentes ocorridos em grandes obras da engenharia como deslizamentos durante a construção do Canal do Panamá ? deixaram clara a percepção inadequada dos princípios de gestão do solo, outrora vigentes na engenharia. Por outro lado, a falta de conhecimento impedia que uma nova direção fosse adotada. Desses desafios e do aprendizado nasceu uma nova orientação do estudo dos solos, que a 7ª edição de Mecânica dos Solos e Suas Aplicações reúne de forma consistente e atualizada.",
         "Sétima",
         ["engenharia", "civil", "geografia"],
-        "assets/images/mecanica_dos_solos_e_suas_aplicacoes.jpg")
+        "assets/images/mecanica_dos_solos_e_suas_aplicacoes.jpg"),
+    Livro(
+        "Design de identidade da marca: Guia Essencial para Toda a Equipe de Gestão de Marcas",
+        "DS001",
+        ["Alina Wheeler"],
+        2000,
+        "Design de Identidade da Marca apresenta um método em cinco fases, universal e comprovado, para criar e implementar uma identidade de marca de sucesso. Da pesquisa e análise à estratégia de marca, do desenvolvimento do design à sua aplicação e de padrões de identidade ao lançamento e à governança, este livro é uma referência essencial tanto para a criação de novas marcas quanto para a revitalização de antigas. Enriquecido com 50 estudos de caso, que descrevem metas, processos, estratégias, soluções e resultados.",
+        "Quinta",
+        ["design", "marketing", "marcas"],
+        "assets/images/design_de_identidade_da_marca_guia_essencial_para_toda_a_equipe_de_gestão_de_marcas.jpg"),
+    Livro(
+        "Criatividade S.A.: Superando as forças invisíveis que ficam no caminho da verdadeira inspiração.",
+        "DS002",
+        ["Amy Wallace", " Edwin Catmull"],
+        2014,
+        "Qual a fórmula por trás de filmes adorados por multidões como Toy Story, Monstros S.A. ou Procurando Nemo? Em Criatividade S.A., Ed Catmull conta a trajetória de sucesso do mais importante e lucrativo estúdio de animação da atualidade, a Pixar, que ele ajudou a fundar, ao lado de Steve Jobs e John Lasseter, em 1986. Dos encontros da equipe às sessões de brainstorm, Catmull mostra como se constrói uma cultura da criatividade, num livro definitivo para quem busca inspiração para os próprios negócios. Para Catmull, a tecnologia jamais deve estar acima de uma boa história; cercar-se de gente mais inteligente do que você torna o ambiente mais propenso à inovação; e encontrar o lado positivo do fracasso é fundamental para garantir o próximo sucesso. Esses são alguns dos princípios apresentados por Catmull em Criatividade S.A. Quase 20 anos depois do lançamento do primeiro filme da trilogia Toy Story, Catmull, atual presidente da Pixar e da Walt Disney Animation Studios, narra a trajetória da empresa que revolucionou a indústria de animação cinematográfica. Entremeando conclusões sobre gestão de pessoal com entusiasmadas informações a respeito dos avanços em tecnologia digital, além da história da Pixar e do cinema de animação, Criatividade S.A. apresenta mais do que conselhos de negócios, com trechos descrevendo situações quase como um roteiro cinematográfico.",
+        "Primeira",
+        ["design", "inovação"],
+        "assets/images/criatividade_sa_superando_as_forcas_invisiveis_que_ficam_no_caminho_da_verdadeira_inspiracao.jpg"),
+    Livro(
+        "SPRINT: O método usado no Google para testar e aplicar novas ideias em apenas cinco dias",
+        "AD002",
+        ["Jake Knapp", "John Zeratsky", "Braden Kowitz"],
+        2016,
+        "Como inovar? Por onde começar? Como formar uma boa equipe? Que forma terá uma ideia quando for colocada em prática? São muitas as perguntas que martelam na cabeça de quem precisa tirar ideias do papel, mas já existe um caminho infalível para responder a todas elas rapidamente: o sprint. Método criado pelo designer Jake Knapp no período em que ele trabalhava no Google, era usado para tudo na empresa, do aperfeiçoamento do mecanismo de buscas ao Google Hangouts, com o foco em desenvolver e testar ideias em apenas cinco dias. Knapp se juntou mais tarde a Braden Kowitz e John Zeratsky no Google Ventures, braço da companhia dedicado ao investimento em novos negócios, e juntos eles conduziram e completaram mais de cem sprints em empresas de telefonia, e-commerce, saúde, finanças e muito mais. Sprint serve para equipes de todos os tamanhos, de pequenas startups até os maiores conglomerados, e pode ser aplicado por qualquer um que tenha uma grande oportunidade, problema ou ideia e precise começar a trabalhar já.",
+        "Primeira(Tradução 2017)",
+        ["administração", "inovação"],
+        "assets/images/sprint_o_metodo_usado_no_google_para_testar_e_aplicar_novas_ideias_em_apenas_cinco_dias.jpg"),
+    Livro(
+        "Motores de combustão interna: vol. 2",
+        "EC001",
+        ["Franco Brunetti"],
+        2012,
+        "Livro-texto para o curso de Engenharia, Motores de Combustão Interna é também leitura recomendada para cursos técnicos e bibliografia de atualização para profissionais da área. Edição com qualidade didática e rica ilustração visual, além de elevada quantidade de exercícios que permite ao leitor testar os conhecimentos adquiridos. Pela primeira vez no Brasil é apresentado um livro, separado em dois volumes, atualizado por 47 especialistas que, num esforço hercúleo, utilizaram um método inovador de desenvolver os conteúdos sem perder a docilidade acadêmica da obra inicial do Professor Franco Brunetti. Trata-se do mais completo tratado sobre Motores publicado no Brasil, onde o leitor encontrará os conceitos mais atualizados. Uma obra indispensável a todos que se interessam e desejam conhecer melhor os Motores de Combustão Interna. O Volume 2 apresenta conceitos complementares: 10 - Sistemas de Injeção para Motores Diesel, 11 - Consumo de Ar nos Motores a Quatro Tempos, 12 - Sistemas de Exaustão, 13 - Emissões, 14 - Lubrificação, 15 - Lubrificantes, 16 - Ruído e Vibrações, 17 - Cinemática e Dinâmica do Motor, 18 - Tribologia, 19 - Sistemas de Arrefecimento, 20 - Projeto de Motores, 21 - Veículos Híbridos.",
+        "Primeira",
+        ["engenharia", "mecânica"],
+        "assets/images/motores_de_combustao_interna_vol_2.jpg"),
+    Livro(
+        "Introdução a Sistemas Elétricos de Potência: Componentes Simétricas",
+        "EE001",
+        [
+          "Carlos César Barioni de Oliveira",
+          "Hernán Prieto Schmidt",
+          "Nelson Kagan",
+          "Ernesto João Robba"
+        ],
+        2000,
+        "Circuitos Trifásicos, definição, resolução de redes trifásicas simétricas e equilibradas. Estudo de desequilíbrios, modelagem da rede e técnicas de solução. Redes trifásicas com impedâncias mútuas, assimétricas e desequilibradas. Modelos de representação de cargas. Valores por Unidade, conceitos gerais, aplicação a circuitos monofásicos. Circuitos trifásicos, representação dos componentes da rede, linhas e transformadores. Vantagens de valores p.u. na representação numérica de redes. Componentes Simétricas, matrizes de transformação, interpretação. Representação dos elementos de redes por diagramas sequenciais e sua associação. Estudo de cargas desequilibradas e de redes com defeitos entre fases, entre fases e terra, abertura monopolar e bipolar. Componentes de Clarke, matrizes de transformação, interpretação. Representação de redes e estudo de desequilíbrios. Defeitos entre fases e entre fases e terra. Exercícios, sistemas de programas computacionais, de domínio público, para a resolução de exercícios passo a passo. Exercícios resolvidos e propostos.",
+        "Segunda",
+        ["engenharia", "elétrica", "controle automação", "eletrônica"],
+        "assets/images/introducao_a_sistemas_eletricos_de_potencia_componentes_simetricas.jpg"),
+    Livro(
+        "Sistema Toyota de Produção: Uma Abordagem Integrada ao Just in Time",
+        "EP001",
+        ["Yasuhiro Monden"],
+        1998,
+        "Um best seller de quase três décadas, este livro mergulha nas práticas do Sistema Toyota de Produção, incluindo bases teóricas e métodos de implementação. A edição clássica está aqui atualizada com novos materiais. Figura fundamental no processo de introdução do Just-in-Time nos Estados Unidos, Yasuhiro Monden explica lógica e a metodologia do STP.",
+        "Quarta",
+        ["engenharia", "produção", "administração"],
+        "assets/images/sistema_toyota_de_producao_uma_abordgem_integrada_ao_just_in_time.jpg"),
   ];
 
   //Construtor
