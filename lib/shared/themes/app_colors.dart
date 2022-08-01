87089de5bc8685f9871d9ac85ccf7b55e7e30b8f
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-31 23:02:49 -0300

    cores -> Primas

--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const darkblue = Color.fromRGBO(0, 70, 132, 1);
-  static const greyblue = Color.fromRGBO(145, 176, 202, 1);
+  static final backgroundColor = Color(0xFFE5E5E5);
+  static final darkblue = Color(0xFF004684);
+  static final lightblue = Color(0xFF86B5D9);
   static const emposse = Color.fromRGBO(51, 0, 255, 1);
   static const devolvido = Color.fromRGBO(8, 204, 51, 1);
   static const atrasado = Color.fromRGBO(204, 8, 43, 1);
+  static final greyblue = Color(0xFF91B0CA);
+  static final reservado = Color(0xFF526E72);
+  static final retirar = Color(0xFF8A00);
 }
