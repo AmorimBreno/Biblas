03c2b08f54373114c88acb9b69fe43ca6c996d96
Breno <brenoamorim11@gmail.com>
2022-08-02 13:04:55 -0300

    Pagina Livros responsiva com as infos dos livros

---
+++ lib/modules/livros/pages/paginaLivrosResponsiva.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:projeto_biblas/database/modules/livro/livro.dart';
+import 'package:projeto_biblas/modules/busca/widgets/iconeProcessos_widget.dart';
+import 'package:projeto_biblas/modules/livros/widgets/botoesPegarLivro_widget.dart';
+import 'package:projeto_biblas/modules/livros/widgets/infoLivro_widget.dart';
+import 'package:projeto_biblas/shared/widgets/app_bar_widget.dart';
+
+class PaginaLivrosResponsiva extends StatefulWidget {
+  const PaginaLivrosResponsiva({Key? key, required this.livro})
+      : super(key: key);
+
+  final Livro livro;
+
+  @override
+  State<PaginaLivrosResponsiva> createState() =>
+      // ignore: no_logic_in_create_state
+      _PaginaLivroResponsivaState(livro: livro);
+}
+
+class _PaginaLivroResponsivaState extends State<PaginaLivrosResponsiva> {
+  _PaginaLivroResponsivaState({required this.livro});
+
+  final Livro livro;
+
+  @override
+  Widget build(BuildContext context) {
+    final larguratela = MediaQuery.of(context).size.width;
+    final alturatela = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        backgroundColor:
+            larguratela < 600 ? Colors.deepPurple[300] : Colors.green[300],
+        body: ListView(
+          children: [
+            const AppBarMaua(),
+            Center(
+                child: SizedBox(
+                    height: alturatela,
+                    width: larguratela > 600 ? 600 : larguratela,
+                    child: InfoLivrosWidget(livro: livro)))
+          ],
+        ));
+  }
+}
