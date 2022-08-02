4c85eabdc342d45ca06808864363f904d296020e
Breno <brenoamorim11@gmail.com>
2022-08-02 14:59:58 -0300

    Pagina Livros terminada a responsividade

--- lib/modules/livros/pages/paginaLivrosResponsiva.dart
+++ lib/modules/livros/pages/paginaLivrosResponsiva.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -6,6 +8,7 @@ import 'package:projeto_biblas/modules/busca/widgets/iconeProcessos_widget.dart'
 import 'package:projeto_biblas/modules/livros/widgets/botoesPegarLivro_widget.dart';
 import 'package:projeto_biblas/modules/livros/widgets/infoLivro_widget.dart';
 import 'package:projeto_biblas/modules/livros/widgets/resumoLivro_widget.dart';
+import 'package:projeto_biblas/modules/meus_processos/pages/processos_pagina.dart';
 import 'package:projeto_biblas/shared/widgets/app_bar_widget.dart';
 
 class PaginaLivrosResponsiva extends StatefulWidget {
@@ -35,20 +38,32 @@ class _PaginaLivroResponsivaState extends State<PaginaLivrosResponsiva> {
       children: [
         const AppBarMaua(),
         Center(
-            child: Padding(
-          padding: const EdgeInsets.only(top: 24.0),
+            child: Expanded(
           child: SizedBox(
-              height: larguratela > 800 ? alturatela * 1.2 : alturatela * 0.9,
+              height: larguratela > 800 ? alturatela * 1.3 : alturatela * 0.9,
               width: larguratela > 800 ? 800 : larguratela,
               child: Column(
                 children: [
+                  const Align(
+                      alignment: Alignment.centerRight,
+                      child: IconeProcessosWidget()),
                   InfoLivrosWidget(livro: livro),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      BotoesLivros(texto: "Solicitar"),
-                      BotoesLivros(texto: "Reservar"),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 32.0, bottom: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        BotoesLivros(texto: "Solicitar"),
+                        BotoesLivros(texto: "Reservar"),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Divider(
+                      thickness: 2,
+                      color: Colors.grey,
+                    ),
                   ),
                   Expanded(child: Sinopse_Widget(livro: livro)),
                 ],
