e82126b76bb09a4a469b1c3278c69c2fde3298e8
Enzo Sakamoto <21.00210-0@maua.br>
2022-08-09 22:42:46 -0300

    FORGET EV EVERYTHING ITS FUCKING BIBLAS

--- lib/database/usuario/usuario_singleton.dart
+++ lib/database/usuario/usuario_singleton.dart
@@ -7,12 +7,14 @@ import '../modules/livro/livro.dart';
 class UsuarioSingleton {
   RepositoryMock repositorio = RepositoryMock();
   late List<LivroUsuario> livros = [
-    LivroUsuario(repositorio.livros[0], DateTime.utc(2022, 2, 04),
-        DateTime.utc(2022, 2, 11), LivroSituacao.EMPOSSE),
-    LivroUsuario(repositorio.livros[1], DateTime.utc(2022, 2, 05),
-        DateTime.utc(2022, 2, 12), LivroSituacao.DEVOLVIDO),
-    LivroUsuario(repositorio.livros[2], DateTime.utc(2022, 2, 06),
-        DateTime.utc(2022, 2, 13), LivroSituacao.ATRASADO),
+    LivroUsuario(repositorio.livros[9], DateTime.utc(2022, 4, 07),
+        DateTime.utc(2022, 4, 14), LivroSituacao.DEVOLVIDO),
+    LivroUsuario(repositorio.livros[11], DateTime.utc(2022, 5, 11),
+        DateTime.utc(2022, 5, 18), LivroSituacao.DEVOLVIDO),
+    LivroUsuario(repositorio.livros[10], DateTime.utc(2022, 8, 05),
+        DateTime.utc(2022, 8, 12), LivroSituacao.ATRASADO),
+    LivroUsuario(repositorio.livros[8], DateTime.utc(2022, 8, 10),
+        DateTime.utc(2022, 8, 17), LivroSituacao.EMPOSSE),
   ];
 
   //Criação do Singleton
